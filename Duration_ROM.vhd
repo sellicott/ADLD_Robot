131eaa22b3a9dd84e843190e33e08f047aa750be
Sam Ellicott <sellicott@cedarville.edu>
2019-03-12 08:00:00 -0400

    tried polyphonic music, not working

--- Duration_ROM.vhd
+++ Duration_ROM.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 
 Entity Duration_ROM is 
 	port(Data_in: in  STD_LOGIC_VECTOR (2 downto 0);
-	     Dur: out STD_LOGIC_VECTOR (5 downto 0));
+	     Dur: out STD_LOGIC_VECTOR (4 downto 0));
 End Duration_ROM ;
 
 Architecture Structure of Duration_ROM  is
@@ -12,14 +12,14 @@ Begin
 	Begin
 		Case Data_in is
 		--   ROM for Note Durations           
-			when "000" => Dur <= "000011";	-- 1/16 Note
-			when "001" => Dur <= "------";	-- 1/8 Note
-			when "010" => Dur <= "001011";	-- 1/4 Note
-			when "011" => Dur <= "------";	-- Dotted 1/4 Note 
-			when "100" => Dur <= "010111";	-- Half Note
-			when "101" => Dur <= "------";	-- Dotted 1/2 Note
-			when "110" => Dur <= "000111";	-- Dotted 1/8 Note
-			when "111" => Dur <= "000100";	-- Triplet 1/12 Note
+			when "000" => Dur <= "00011";	-- 1/16 Note
+			when "001" => Dur <= "-----";	-- 1/8 Note
+			when "010" => Dur <= "01011";	-- 1/4 Note
+			when "011" => Dur <= "-----";	-- Dotted 1/4 Note 
+			when "100" => Dur <= "10111";	-- Half Note
+			when "101" => Dur <= "-----";	-- Dotted 1/2 Note
+			when "110" => Dur <= "00111";	-- Dotted 1/8 Note
+			when "111" => Dur <= "00100";	-- Triplet 1/12 Note
 		End Case;
 	End process;
 End Structure;
--- Notes.vhd
+++ Notes.vhd
@@ -13,16 +13,16 @@ Begin
 	Begin
 		Case Data_in is
 when "1100" =>Freq <="01100011";  --b 99
-when "1011" =>Freq <="01101001";
+when "1011" =>Freq <="--------";
 when "1010" =>Freq <="01101111";
 when "1001" =>Freq <="01110110";
-when "1000" =>Freq <="01111101";
+when "1000" =>Freq <="--------";
 when "0111" =>Freq <="10000100";
-when "0110" =>Freq <="10001100";
+when "0110" =>Freq <="--------";
 when "0101" =>Freq <="10010100";
 when "0100" =>Freq <="10011101";
 when "0011" =>Freq <="10100110";
-when "0010" =>Freq <="10110000";  -- c# 176
+when "0010" =>Freq <="--------";  -- c# 176
 when "0001" =>Freq <="10111011";  -- Middle C 187
 when "0000" =>Freq <="00000000";  --rest all zeros
 
